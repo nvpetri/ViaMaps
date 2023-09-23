01d4f089ff562fac94d5095e83412a128a5dcd93
nvpetri <nicolaspetri18@gmail.com>
2023-09-22 22:48:05 -0300

    desktop e script pronto

--- app.js
+++ app.js
@@ -3,19 +3,19 @@
 const cepInput = document.getElementById('cep')
 const buscar = document.getElementById('buscar')
 
-async function pegarEndereco (cep){
-     const url = `https://viacep.com.br/ws/${cep}/json/`
-     const response = await fetch(url)
-     const endereco = await response.json()
-     console.log(url)
-     return endereco
+async function pegarEndereco(cep) {
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const response = await fetch(url)
+    const endereco = await response.json()
+    console.log(url)
+    return endereco
 }
 
-async function preencherCampos(){
+async function preencherCampos() {
     const rua = document.getElementById('rua')
     const cidade = document.getElementById('cidade')
     const estado = document.getElementById('estado')
-    
+
     const cepInfo = await pegarEndereco(cepInput.value)
 
     rua.value = cepInfo.logradouro
@@ -23,4 +23,16 @@ async function preencherCampos(){
     estado.value = cepInfo.uf
 }
 
+async function mudarLocalizacao() {
+
+    const cepInfo = await pegarEndereco(cepInput.value)
+
+    const rua = cepInfo.logradouro
+    const cidade = cepInfo.localidade
+    const estado = cepInfo.uf
+
+    const localizacao = document.getElementById('iframe').src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAeVRmm_pxeTiNMChEeLFAJxC8nR-X6Mj4&q=" + rua + "," + cidade + "+" + estado
+    console.log(localizacao)
+}
 buscar.addEventListener('click', preencherCampos)
+buscar.addEventListener('click', mudarLocalizacao)
